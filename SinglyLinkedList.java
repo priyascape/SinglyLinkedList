0829cab5d58029df9d6ec88618744606b5195aa3
priyascape <priyascape@gmail.com>
2021-04-06 01:17:26 +0100

    SinglyLinkedList

---
+++ SinglyLinkedList.java
@@ -0,0 +1,40 @@
+public class SinglyLinkedList {
+    public Node head;
+    public SinglyLinkedList() {
+        this.head = null;
+    }
+    // Methods
+    public void add(int value) {
+        Node newNode = new Node(value);
+        if(head == null) {
+            head = newNode;
+        } else {
+            Node runner = head;
+            while(runner.next != null) {
+                runner = runner.next;
+            }
+            runner.next = newNode;
+        }
+    }    
+    public void remove() {
+        if (head.next == null) {
+            this.head = null;
+        }
+            Node runner = this.head;
+            while (runner.next.next != null) {
+                runner = runner.next;
+            }
+            runner.next = null;
+    }
+    public void printValues(){
+        Node runner = this.head;
+        if(head == null){
+            System.out.println("No nodes!");
+        }
+        while(runner.next != null){
+            System.out.println(runner.value);
+            runner = runner.next;
+        }
+        System.out.println(runner.value);
+    }
+}
